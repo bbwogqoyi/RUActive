09befe987458ec6ef51d382a79207895e4823afa
Valentine Mthembu <mthembuval@gmail.com>
2017-10-06 01:04:53 +0200

    updated html

---
+++ webapp/Assets/javascript/logUserIn.js
@@ -0,0 +1,28 @@
+function login(){
+
+  var username = document.getElementById('lg_username').value;
+  var password = document.getElementById('lg_password').value;
+  
+  if(username !== null && username !== "" && password !== null && password !== ""){
+
+  if(document.getElementById('xip_RUActive_Users_Staff_Student_ID')==null)
+  moveToNextPage();
+ else{
+  document.getElementById('xip_RUActive_Users_Staff_Student_ID').value = username;
+  document.getElementById('xip_RUActive_Users_Password').value = password
+  document.getElementById('caspioform').submit(); 
+ }
+  
+  
+  }
+  else{
+	  if(username == "" && password != "" ) alert("username is empty");
+	  if(password == "" && username != "") alert("password is empty");
+	  if(username == "" && password =="") alert("username and password is empty");
+  }
+
+}
+
+function moveToNextPage(){
+  location.href = "index.html"
+}
