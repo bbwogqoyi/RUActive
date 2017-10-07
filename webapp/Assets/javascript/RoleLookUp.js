d0932559cb893e2ad0575f223bc88f6550b7e707
Valentine Mthembu <mthembuval@gmail.com>
2017-10-07 23:53:26 +0200

    no message

--- webapp/Assets/javascript/RoleLookUp.js
+++ webapp/Assets/javascript/RoleLookUp.js
@@ -15,17 +15,17 @@ function userRoleLookUp(num){
 }
 
 
-function expertise(exp){
+function expertiseLookup(exp){
   var role = -1;
   //position = parseInt(num);
   switch(exp){
-    case Novice Player:
+    case "Novice Player":
       role = 1; break;
-    case Competent Playe:
+    case "Competent Playe":
       role  = 2; break;
-    case RU First Team Player:
+    case "RU First Team Player":
       role = 3; break;
-    case Provincial/National Player:
+    case "Provincial/National Player":
       role = 4; break;
   }
   return role;
