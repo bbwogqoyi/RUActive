c3bd4e776c6c018bbcf81638f8dac70084f930cf
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2017-10-09 10:52:19 +0200

    non_fixture_score

--- functions/fixtures.js
+++ functions/fixtures.js
@@ -1,21 +1,7 @@
 'use strict'
 
 fixtures = {
-  positionToPoints(position){
-    var points = 0;
-    position = parseInt(position);
-    switch(position){
-      case 1:
-        points += 8; break;
-      case 2:
-        points += 6; break;
-      case 3:
-        points += 4; break;
-      case 4:
-        points += 2; break;
-    }
-    return points;
-  },
+  ,
 
 }
 
